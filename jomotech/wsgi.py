25c78fc045765968b68ec30cd9fd98186e0bc833
mukule <nelsonmasibo6@gmail.com>
2024-04-09 15:54:21 +0300

    first commit

---
+++ jomotech/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jomotech project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jomotech.settings')
+
+application = get_wsgi_application()
